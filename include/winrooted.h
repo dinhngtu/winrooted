730cb9db340f8e1595169fc1c9db5f0fcb0905c2
Tu Dinh <1257909+dinhngtu@users.noreply.github.com>
2026-07-12 14:25:44 +0200

    add WinrootedDoInRoot
    Signed-off-by: Tu Dinh <1257909+dinhngtu@users.noreply.github.com>

--- include/winrooted.h
+++ include/winrooted.h
@@ -14,3 +14,23 @@ EXTERN_C HRESULT WinrootedCreateFileAt(
     _In_ DWORD creationDisposition,
     _In_ DWORD flags,
     _In_ DWORD attributes) WIN_NOEXCEPT;
+
+// `WINROOTED_IN_ROOT_FUNC` must not throw a C++ exception. Doing so is fatal.
+typedef HRESULT (*WINROOTED_IN_ROOT_FUNC)(
+    // Handle of directory containing the last path element. Do not close this handle.
+    _In_ HANDLE parent,
+    // Name of the last path element.
+    // Function may return `HRESULT_FROM_WIN32(ERROR_REPARSE_POINT_ENCOUNTERED)` to indicate that `name` is a symlink
+    // which should be followed.
+    _In_ PCWSTR name,
+    // Context passed from `winrooted_Root_DoInRoot`.
+    _Inout_opt_ PVOID context,
+    // Path to symlink to be followed.
+    // Must be valid iff function returns `HRESULT_FROM_WIN32(ERROR_REPARSE_POINT_ENCOUNTERED)`. Not doing so is fatal.
+    // The link string will be freed with `free()`.
+    _When_(return == __HRESULT_FROM_WIN32(ERROR_REPARSE_POINT_ENCOUNTERED), _Post_valid_ _At_(*link, _Post_notnull_))
+        PWSTR *link);
+
+EXTERN_C HRESULT
+WinrootedDoInRoot(_In_ HANDLE dirfd, _In_ PCWSTR path, _In_ WINROOTED_IN_ROOT_FUNC func, _Inout_opt_ PVOID context)
+    WIN_NOEXCEPT;
--- src/root.cpp
+++ src/root.cpp
@@ -45,3 +45,42 @@ EXTERN_C HRESULT WinrootedCreateFileAt(
     return S_OK;
 }
 CATCH_RETURN();
+
+static std::variant<HRESULT, std::wstring> WinrootedDoInRootOne(
+    _In_ HANDLE parent,
+    _In_ std::wstring_view name,
+    _In_ WINROOTED_IN_ROOT_FUNC func,
+    _Inout_opt_ PVOID context) WIN_NOEXCEPT try {
+    std::wstring namestr(name);
+    PWSTR newlink = nullptr;
+    HRESULT hr;
+
+    try {
+        hr = func(parent, namestr.c_str(), context, &newlink);
+    }
+    CATCH_FAIL_FAST_MSG("WINROOTED_IN_ROOT_FUNC threw an unhandled exception");
+
+    if (hr == HRESULT_FROM_WIN32(ERROR_REPARSE_POINT_ENCOUNTERED)) {
+        FAIL_FAST_IF_NULL_MSG(
+            newlink,
+            "WINROOTED_IN_ROOT_FUNC returned a null link with ERROR_REPARSE_POINT_ENCOUNTERED");
+        auto newlink_free = wil::scope_exit([=]() { free(newlink); });
+        return std::wstring(newlink);
+    } else {
+        return hr;
+    }
+}
+CATCH_RETURN();
+
+EXTERN_C HRESULT
+WinrootedDoInRoot(_In_ HANDLE dirfd, _In_ PCWSTR path, _In_ WINROOTED_IN_ROOT_FUNC func, _Inout_opt_ PVOID context)
+    WIN_NOEXCEPT try {
+    return winrooted::DoInRoot<HRESULT>( //
+        dirfd,
+        path,
+        nullptr,
+        [=](HANDLE parent, std::wstring_view name) {
+            return WinrootedDoInRootOne(parent, name, func, context);
+        });
+}
+CATCH_RETURN();
